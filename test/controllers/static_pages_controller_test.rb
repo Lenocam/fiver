847acec84743f5922ed4f82e083b35b9bdec0bd8
Reed MacConnell <Learn22Ruby@gmail.com>
2016-02-09 18:23:50 -0500

    created static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,16 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
-    assert_response :success
-  end
+	test "should get home" do
+		get :home
+		assert_response :success
+		assert_select "title", "Home | Fiver"
+	end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-  end
+	test "should get help" do
+		get :help
+		assert_response :success
+		assert_select "title", "Help | Fiver"
+	end
 
 end
